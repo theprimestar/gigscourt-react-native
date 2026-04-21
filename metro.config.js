a946eacb54bde18d1a9366565a1a3ca3ef5fb10f
Gigscourt <theprimestarventures@gmail.com>
2026-04-21 17:01:44 +0100

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// This is the key addition: it explicitly tells Metro how to treat certain asset files
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+
+config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
+
 module.exports = config;
