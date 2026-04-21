9d6d2953abaf41c238814f222ad7fc2b57de4cff
Gigscourt <theprimestarventures@gmail.com>
2026-04-21 16:03:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/**
- * @format
- */
-
-import { AppRegistry } from 'react-native';
+import { registerRootComponent } from 'expo';
 import App from './App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// Register the main component with Expo
+registerRootComponent(App);
